aca399d4d538bd610ecc0663bb542d088cbb998d
Arslan <ali.arslan319@gmail.com>
2021-11-27 23:02:45 -0500

    change borderColor and background if incorrect input is entered with inline styling

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -24,7 +24,11 @@ const CourseInput = props => {
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input 
+        style={{
+          background: !isValid ? 'red' : '#fad0ec', 
+          borderColor: !isValid ? 'salmon' : 'transparent'}} 
+          type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
