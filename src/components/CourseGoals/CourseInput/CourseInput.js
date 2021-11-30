7a08a096db89d90d9b1f4f5b01829c80bb1f8d3f
Arslan <ali.arslan319@gmail.com>
2021-11-29 23:48:39 -0500

    color styling option adding to default styled.div

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,14 +5,18 @@ import './CourseInput.css';
 
 const FormControl = styled.div`
   margin: 0.5rem 0;
+  {
   font-weight: bold;
   display: block;
   margin-bottom: 0.5rem;
+  color: ${props => props.invalid ? 'red' : 'black'}
+  }
 
 & input {
   display: block;
   width: 100%;
-  border: 1px solid #ccc;
+  border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
+  background: ${props => props.invalid ? '#ffd7d7' : 'transparent'}
   font: inherit;
   line-height: 1.5rem;
   padding: 0 0.25rem;
@@ -22,15 +26,6 @@ const FormControl = styled.div`
   outline: none;
   background: #fad0ec;
   border-color: #8b005d;
-}
-
-& invalid input {
-  border-color: red;
-  background: #ffd7d7;
-}
-
-& invalid label {
-  color: red;
 }
 `
 
@@ -56,7 +51,8 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl>
+      {/* <FormControl className={!isValid && 'invalid'}> */}
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </FormControl>
